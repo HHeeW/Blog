713ff6ec5247a63610c95bf79949eeb79cd48d92
HeeWon <sun4472@naver.com>
2022-11-05 19:54:23 +0900

    2022-11-05

--- hhw/src/Firebase.js
+++ hhw/src/Firebase.js
@@ -3,18 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_APIKEY,
-  // authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  // projectId: process.env.REACT_APP_PROJECTID,
-  // storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  // messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  // appId: process.env.REACT_APP_APPID
-  apiKey: "AIzaSyBxTPPZCo20Zraw3dK1K-wPGn394tmnnsw",
-  authDomain: "hheewon-9590f.firebaseapp.com",
-  projectId: "hheewon-9590f",
-  storageBucket: "hheewon-9590f.appspot.com",
-  messagingSenderId: "42258298290",
-  appId: "1:42258298290:web:89683131989410933ce0ab"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
 };
 
 const app = initializeApp(firebaseConfig);
